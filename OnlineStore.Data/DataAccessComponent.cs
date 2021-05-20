defecf61405984f6a7fc626aad72565b3b71e7c4
JavierMaldonadoSantiago <javier.maldonado.santiago@outlook.es>
2021-05-20 12:20:22 -0500

    Se agrego estructura de base de datos

---
+++ OnlineStore.Data/DataAccessComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace OnlineStore.Data
+{
+    public class DataAccessComponent
+    {
+        protected const string CONNECTION_NAME = "DigitalizacionEntities";
+
+
+        protected StringBuilder log = new StringBuilder();
+        protected string ConnectionString
+        {
+            get
+            {
+
+                return ConfigurationManager.ConnectionStrings[CONNECTION_NAME].ConnectionString;
+            }
+        }
+    }
+}
